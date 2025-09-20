1ab03ad89c418fd756058f5d5a4736caa6bf40b1
GohanVu <hoangvhdata@gmail.con>
2025-09-20 22:57:42 +0700

    20250920 nhan dien thong tin duong link

--- src/platform_detector.py
+++ src/platform_detector.py
@@ -4,6 +4,7 @@ Phát hiện và quản lý các platform được hỗ trợ
 """
 
 from .constants import PLATFORMS
+from .link_detector import LinkDetector
 
 
 class PlatformDetector:
@@ -11,10 +12,11 @@ class PlatformDetector:
     
     def __init__(self):
         self.platforms = PLATFORMS
+        self.link_detector = LinkDetector()
         
     def detect_platform(self, url):
         """
-        Phát hiện platform từ URL
+        Phát hiện platform từ URL (sử dụng LinkDetector mới)
         
         Args:
             url (str): URL cần phát hiện
@@ -25,23 +27,25 @@ class PlatformDetector:
         if not url or not url.strip():
             return None
             
-        url_lower = url.lower()
+        # Sử dụng LinkDetector để phân tích chi tiết
+        link_info = self.link_detector.detect_link_type(url)
         
-        for platform_key, platform_info in self.platforms.items():
-            for keyword in platform_info['keywords']:
-                if keyword in url_lower:
-                    return {
-                        'key': platform_key,
-                        'name': platform_info['name'],
-                        'emoji': platform_info['emoji'],
-                        'info': platform_info
-                    }
+        if link_info['is_supported'] and link_info['platform_info']:
+            return {
+                'key': link_info['platform_info']['key'],
+                'name': link_info['platform_info']['name'],
+                'emoji': link_info['platform_info']['emoji'],
+                'link_type': link_info['link_type'],
+                'content_id': link_info['content_id'],
+                'description': link_info['description'],
+                'info': self.platforms[link_info['platform_info']['key']]
+            }
         
         return None
         
     def get_platform_display_text(self, url):
         """
-        Lấy text hiển thị cho platform
+        Lấy text hiển thị cho platform với thông tin chi tiết
         
         Args:
             url (str): URL cần phát hiện
@@ -58,9 +62,31 @@ class PlatformDetector:
         if platform['key'] == 'douyin':
             display_text = f"{platform['emoji']} {platform['name']} đang phát triển"
         else:
-            display_text = f"{platform['emoji']} {platform['name']}"
+            # Hiển thị thông tin chi tiết về loại link
+            link_type_info = self._get_link_type_display(platform['link_type'])
+            display_text = f"{platform['emoji']} {platform['name']} - {link_type_info}"
             
         return display_text, self._get_success_style()
+        
+    def _get_link_type_display(self, link_type):
+        """Chuyển đổi link type thành text hiển thị"""
+        type_mapping = {
+            'video': 'Video',
+            'shorts': 'Shorts',
+            'reel': 'Reel',
+            'live': 'Live',
+            'music': 'Music',
+            'playlist': 'Playlist',
+            'channel': 'Channel',
+            'profile': 'Profile',
+            'post': 'Post',
+            'igtv': 'IGTV',
+            'stories': 'Stories',
+            'shortlink': 'Shortlink',
+            'watch_page': 'Watch Page',
+            'share': 'Share Link'
+        }
+        return type_mapping.get(link_type, 'Content')
         
     def get_default_display(self):
         """Lấy hiển thị mặc định khi chưa có URL"""
@@ -94,6 +120,38 @@ class PlatformDetector:
             }
             for key, info in self.platforms.items()
         ]
+        
+    def get_detailed_link_info(self, url):
+        """
+        Lấy thông tin chi tiết về link
+        
+        Args:
+            url (str): URL cần phân tích
+            
+        Returns:
+            dict: Thông tin chi tiết về link
+        """
+        return self.link_detector.detect_link_type(url)
+        
+    def is_playlist_link(self, url):
+        """Kiểm tra có phải link playlist không"""
+        return self.link_detector.is_playlist_link(url)
+        
+    def is_channel_link(self, url):
+        """Kiểm tra có phải link channel/profile không"""
+        return self.link_detector.is_channel_link(url)
+        
+    def is_live_link(self, url):
+        """Kiểm tra có phải link live stream không"""
+        return self.link_detector.is_live_link(url)
+        
+    def extract_video_id(self, url):
+        """Trích xuất video ID từ URL"""
+        return self.link_detector.extract_video_id(url)
+        
+    def get_supported_link_types(self):
+        """Lấy danh sách các loại link được hỗ trợ"""
+        return self.link_detector.get_supported_link_types()
         
     def _get_default_style(self):
         """Style mặc định cho platform display"""
